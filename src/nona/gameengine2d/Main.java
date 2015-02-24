5e036bb9220317a163a7d55e54743ba65c78cc0d
LeonardVollmann <vollmann.leonard@gmail.com>
2015-02-24 21:57:40 +0100

    Added a window to the CoreEngine

--- src/nona/gameengine2d/Main.java
+++ src/nona/gameengine2d/Main.java
@@ -5,7 +5,7 @@ import nona.gameengine2d.core.CoreEngine;
 public class Main {
 
 	public static void main(String[] args) {
-		CoreEngine engine = new CoreEngine(720, 540, "2D Game Engine", 60);
+		CoreEngine engine = new CoreEngine(1080, 720, "2D Game Engine", 60);
 		
 		engine.start();
 	}
--- src/nona/gameengine2d/core/CoreEngine.java
+++ src/nona/gameengine2d/core/CoreEngine.java
@@ -1,6 +1,14 @@
 package nona.gameengine2d.core;
 
-public class CoreEngine implements Runnable {
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.nio.ByteBuffer;
+
+import org.lwjgl.glfw.GLFWvidmode;
+
+public class CoreEngine {
 
 	public static final boolean IGNORE_FRAMECAP = true;
 	
@@ -8,7 +16,15 @@ public class CoreEngine implements Runnable {
 	
 	private double nsPerUpdate;
 	
+	private long window;
+	private int width;
+	private int height;
+	private String title;
+	
 	public CoreEngine(int width, int height, String title, int fps) {
+		this.width = width;
+		this.height = height;
+		this.title = title;
 		this.nsPerUpdate = 1000000000.0 / fps;
 		
 		this.running = false;
@@ -16,6 +32,7 @@ public class CoreEngine implements Runnable {
 	
 	public void start() {
 		running = true;
+		init();
 		run();
 	}
 	
@@ -23,8 +40,7 @@ public class CoreEngine implements Runnable {
 		running = false;
 	}
 	
-	@Override
-	public void run() {
+	private void run() {
 		long lastTime = System.nanoTime();
 		long now;
 		double delta = 0;
@@ -36,6 +52,11 @@ public class CoreEngine implements Runnable {
 		boolean shouldRender = false;
 		
 		while (running) {
+			if (glfwWindowShouldClose(window) == GL_TRUE) {
+				running = false;
+				continue;
+			}
+			
 			now = System.nanoTime();
 			delta += now - lastTime;
 			lastTime = now;
@@ -63,11 +84,33 @@ public class CoreEngine implements Runnable {
 	}
 	
 	private void update(float delta) {
-		
+		glfwPollEvents();
 	}
 	
 	private void render() {
+		glfwSwapBuffers(window);
+	}
+	
+	private void init() {
+		if (glfwInit() != GL_TRUE) {
+			// TODO: Proper Error handling
+			System.err.println("ERROR: Failed to initialise GLFW.");
+			System.exit(1);
+		}
 		
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		window = glfwCreateWindow(width, height, title, NULL, NULL);
+		if (window == NULL) {
+			// TODO: Proper Error handling
+			System.err.println("ERROR: Failed to create window.");
+			System.exit(1);
+		}
+		
+		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - width) / 2, (GLFWvidmode.height(vidmode) - height) / 2);
+		
+		glfwMakeContextCurrent(window);
+		glfwShowWindow(window);
 	}
 	
 }
